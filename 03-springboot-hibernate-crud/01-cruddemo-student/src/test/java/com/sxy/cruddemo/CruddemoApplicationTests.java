6726b9064ad3f0b09fea8333002d9a986aa25189
Samuel <kyohn9178@gmail.com>
2023-06-05 22:38:15 +0100

    Set up CRUD app;app.properties.CommandLineRunner

---
+++ 03-springboot-hibernate-crud/01-cruddemo-student/src/test/java/com/sxy/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxy.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
