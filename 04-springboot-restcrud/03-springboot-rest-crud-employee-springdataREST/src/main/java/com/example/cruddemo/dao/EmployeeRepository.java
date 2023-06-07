7c89a205b8dfd43b873ffd20ef76da958465bcb1
Samuel <kyohn9178@gmail.com>
2023-06-07 03:04:45 +0100

    Spring boot data rest:custom endpoint path; other config:pagesize

--- 04-springboot-restcrud/03-springboot-rest-crud-employee-springdataREST/src/main/java/com/example/cruddemo/dao/EmployeeRepository.java
+++ 04-springboot-restcrud/03-springboot-rest-crud-employee-springdataREST/src/main/java/com/example/cruddemo/dao/EmployeeRepository.java
@@ -2,8 +2,10 @@ package com.example.cruddemo.dao;
 
 import com.example.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
+@RepositoryRestResource(path="members")
+//expose "members" for this given REST repository
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
     //no need to write any code here!
 }
